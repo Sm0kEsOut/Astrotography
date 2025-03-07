0f742c2ff5041df01b4b5b688257a8a7b36e8cf7
Brody Gaston <gaston.brody@gmail.com>
2025-03-06 20:29:05 -0800

    Adding directory.js

---
+++ js/directory.js
@@ -0,0 +1,55 @@
+$('document').ready(function () {
+
+    const SS = document.getElementsByClassName("SS");
+    const MW = document.getElementsByClassName("MW");
+    const DS = document.getElementsByClassName("DS");
+
+    if (document.title == 'Astrotography | Directory') {
+
+        SS[0].addEventListener("click", function () {
+            document.getElementById('SS').scrollIntoView();
+        });
+
+        MW[0].addEventListener("click", function () {
+            document.getElementById('MW').scrollIntoView();
+        });
+
+        DS[0].addEventListener("click", function () {
+            document.getElementById('DS').scrollIntoView();
+        });
+
+        if (localStorage.getItem('scrollTo') == "SS") {
+            document.getElementById('SS').scrollIntoView();
+            localStorage.removeItem('scrollTo');
+        } else if (localStorage.getItem('scrollTo') == "MW") {
+            document.getElementById('MW').scrollIntoView();
+            localStorage.removeItem('scrollTo');
+        } else if (localStorage.getItem('scrollTo') == "DS") {
+            document.getElementById('DS').scrollIntoView();
+            localStorage.removeItem('scrollTo');
+        }
+
+    } else {
+
+        SS[0].addEventListener("click", function () {
+            localStorage.removeItem('scrollTo');
+            localStorage.setItem('scrollTo', 'SS');
+            document.location.href = "../directory.html";
+        });
+
+        MW[0].addEventListener("click", function () {
+            localStorage.removeItem('scrollTo');
+            localStorage.setItem("scrollTo", "MW");
+            document.location.href = "../directory.html";
+        });
+
+        DS[0].addEventListener("click", function () {
+            localStorage.removeItem('scrollTo');
+            localStorage.setItem("scrollTo", "DS");
+            document.location.href = "../directory.html";
+        });
+
+    }
+
+});
+
